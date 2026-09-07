4f92a70720ada9a13d7eec76aa8ec6b75c6329dc
Mattia Franco <mattiap200210@gmail.com>
2026-09-07 17:41:33 +0200

    v1.8.7 aggiornamento versione per test updater

--- app/updater.py
+++ app/updater.py
@@ -21,7 +21,7 @@ from PySide6.QtWidgets import QApplication, QMessageBox
 # GESTIONE TURNI - AGGIORNAMENTI
 # =========================================================
 
-CURRENT_VERSION = "1.8.6"
+CURRENT_VERSION = "1.8.7"
 
 GITHUB_OWNER = "Chivas1504"
 GITHUB_REPO = "GestioneTurni"
